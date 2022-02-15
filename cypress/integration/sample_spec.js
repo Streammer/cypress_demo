f1438387f7f4f204f0aaaab76ae8555632942245
Streammer <Lukdmitrij@yandex.ru>
2022-02-15 22:12:01 +0300

    visit oliner

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,12 @@
 describe("My first test", () => {
-    it("Doesn't do much", () => {
-        expect(true).to.equal(true)
+    it("Visit the onliner", () => {
+        cy.visit('https://www.onliner.by/')
+            // cy.contains('type').click()
+            //     // Should be on a new URL which includes '/commands/actions'
+            // cy.url().should('include', '/commands/actions')
+        cy.get('.fast-search__input').click()
+
+
+
     })
 })
